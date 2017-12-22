58fa0236c0aa22cc229fe71a69ff1d6ebbb033af
alexandermxu <alex.m.xu@gmail.com>
2017-12-22 15:02:18 -0800

    Big Edit 12/22/2017

--- ProteinChipProcessing.R
+++ ProteinChipProcessing.R
@@ -1,24 +1,46 @@
-barcodes <- c("B", "C", "D")
-blocks <- unique(AX179T$Block)
-output <- array(dim = c(length(blocks)*length(barcodes), ncol(AX179T)-2))
+library(xlsx)
+library(tidyverse)
+File <- read.xlsx("AX194_Adjusted.xlsx", sheetIndex = 2)
+Cells <- c(1,0,0,0,0,0,2,0,1,0,0,0,
+           0,1,3,1,2,2,2,1,2,0,4,3,
+           0,0,0,1,3,1,0,0,0,0,0,0)
+Barcodes <- c("B", "C", "D")
+Blocks <- unique(File$Block)
+Group1 <- c(1:6, 13:18, 25:30)
+Group2 <- c(7:12, 19:24, 31:36)
+Registry1 <- c(NA,NA,NA,NA,"B","C","D",NA,NA,NA,NA)
+Registry2 <- c(NA,NA,NA,NA,NA,"D","C","B",NA,NA,NA)
 A1TopRight <- c(36,25,24,13,12,1,35,26,23,14,11,2,34,27,22,15,10,3,33,28,21,16,9,4,32,29,20,17,8,5,31,30,19,18,7,6)
-
-numvalues <- length(colnames(AX179T)[-c(1:3,length(colnames(AX179T)))])
-
-values <- AX179T[colnames(AX179T)[-c(1:3,length(colnames(AX179T)))]]
+KeepData <- c("Block", "Row", "F635.Mean", "F532.Mean")
+Entries <- length(KeepData)
+Output <- array(dim = c(length(Blocks)*length(Barcodes), Entries+2))
+FileExtract <- File[,KeepData]
+FileExtract[FileExtract$Block %in% Group1,Entries+1] <- Registry1[FileExtract[FileExtract$Block %in% Group1,]$Row]
+FileExtract[FileExtract$Block %in% Group2,Entries+1] <- Registry2[FileExtract[FileExtract$Block %in% Group2,]$Row]
+ReducedFileExtract <- na.omit(FileExtract)
+colnames(ReducedFileExtract) <- c("Block", "Row", "Mean635", "Mean532", "Barcode")
+Values <- ReducedFileExtract[,-c(1:2,Entries+1)]
 i=1
-for (n in 1:length(blocks))
+for (n in 1:length(Blocks))
 {
-  for (m in 1:length(barcodes))
+  for (m in 1:length(Barcodes))
   {
-    output[i,1] <- blocks[n]
-    output[i,2] <- barcodes[m]
-    output[i,3:18] <- colMeans(values[AX179T$Block==blocks[n] & AX179T$Barcode==barcodes[m],])
+    Output[i,1] <- Blocks[n]
+    Output[i,2] <- Barcodes[m]
+    Output[i,3:Entries] <- colMeans(Values[ReducedFileExtract$Block==Blocks[n] & ReducedFileExtract$Barcode==Barcodes[m],])
     i=i+1
   }
 }
 
-colnames(output) <- colnames(AX179T)[c(-2, -ncol(AX179T))]
-TrueBlock <- A1TopRight[as.double(as.character(output1$Block))]
+Output[,Entries+1] <- A1TopRight[as.numeric(as.character(Output[,1]))]
 
-output <- data.frame(output)
+Output[,Entries+2] <- Cells[as.numeric(as.character(Output[,Entries+1]))]
+colnames(Output) <- c("Block", "Barcode", "Mean635", "Mean532", "Chamber", "Cells")
+# colnames(Output) <- c("Block", "Barcode", "Mean635", "Mean594", "Mean532", "Mean488", "Chamber", "Cells")
+
+Output <- data.frame(Output)
+for (l in c(1,3:(Entries+2)))
+{Output[,l] <- as.numeric(as.character(Output[,l]))}
+
+Plot1 <- ggplot(Output, aes(x=Barcode,y=Mean635,label=Block)) + geom_jitter(aes(color=factor(Cells)))
+Plot1
